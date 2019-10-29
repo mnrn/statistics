c31aac3cda9ece2cc0e1e17dffc46d65121a9f86
ayase <paper.airplane01@gmail.com>
2019-10-29 15:19:28 +0900

    add stats3-3.py

--- stats3-4.py
+++ stats3-4.py
@@ -5,18 +5,22 @@ xs = [71, 68, 66, 67, 70, 71, 70, 73, 72, 65, 66]
 ys = [69, 64, 65, 63, 65, 62, 65, 64, 66, 59, 62]
 
 
-def p2():
-    rs = np.random.randint(0, 11, 11)
+def toi1():
+    return np.random.randint(0, 11, 11)
+
+
+def toi2():
+    rs = toi1()
     rxys = [[xs[r], ys[r]]for r in rs]
     rxs, rys = zip(*rxys)
     co = np.corrcoef(rxs, rys)
     print(co)
 
 
-def p3():
+def toi3():
     cs = []
     for i in range(0, 200):
-        rs = np.random.randint(0, 11, 11)
+        rs = toi1()
         rxys = [[xs[r], ys[r]]for r in rs]
         rxs, rys = zip(*rxys)
         co = np.corrcoef(rxs, rys)
@@ -26,4 +30,5 @@ def p3():
 
 
 if __name__ == '__main__':
-    p3()
+    toi2()
+    toi3()
